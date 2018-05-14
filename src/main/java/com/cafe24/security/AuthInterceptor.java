58f2d87064b4152aaa46353dd3dfc5e44395691d
BIT <BIT@DESKTOP-IMJNJL2>
2018-05-14 14:21:48 +0900

    d

--- src/main/java/com/cafe24/security/AuthInterceptor.java
+++ src/main/java/com/cafe24/security/AuthInterceptor.java
@@ -13,29 +13,17 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		// 1. handler 종류 확인
-		// default handler method일 경우(false)
+			throws Exception {		
 		if(handler instanceof HandlerMethod == false) {
 			return false;
 		}
 		
-		// 2. casting
-		// 어노테이션 정보를 뽑아내기 위해
 		HandlerMethod handlerMethod = (HandlerMethod) handler;
-		
-		// 3. @Auth 받아오기
-		// 없으면 null
 		Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
-		// auth.role(); USER 또는 ADMIN
-		
-		// 4. Method에 @Auth 가 없는  경우
-		// @Auth 없으면 뒤의 Controller로 가서 사용할 핸들러 사용해라
 		if(auth == null) {
 			return true;
 		}
 		
-		// 5. @Auth가 붙어 있는 경우, 인증 여부 체크
 		HttpSession session = request.getSession();
 		if(session == null) {
 			response.sendRedirect(request.getContextPath()+"/user/login");
@@ -48,7 +36,6 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 			return false;
 		}
 		
-		// 6. 접근 허가
 		return true;
 	}
 	
